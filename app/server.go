6db7782757df02ed19a618e8a37fb47a0baef825
BeeOnDWeb <beeondweb@gmail.com>
2024-08-06 21:03:34 +1000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -16,13 +18,26 @@ func main() {
 
 	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	// Channel to listen for interrupt signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
-	handleConnection(conn)
+	go func() {
+		<-sigChan
+		fmt.Println("Shutting down server...")
+		l.Close()
+		os.Exit(0)
+	}()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		go handleConnection(conn)
+	}
 
 }
 
@@ -30,6 +45,24 @@ func handleConnection(conn net.Conn) {
 
 	defer conn.Close()
 
-	conn.Write([]byte("+PONG\r\n"))
+	for {
 
+		buff := make([]byte, 1024)
+
+		n, err := conn.Read(buff)
+		if err != nil {
+			print(err.Error())
+			break
+		}
+
+		data := string(buff[:n])
+
+		fmt.Println("Received data: ", data)
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to connection:", err.Error())
+			break
+		}
+
+	}
 }
